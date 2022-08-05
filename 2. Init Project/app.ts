40cf3acf21a53a9764ac1f62ac35b371ec6cd03e
aathym <aat8001@gmail.com>
2022-08-05 14:22:29 +0530

    Obbject Types

--- 2. Init Project/app.ts
+++ 2. Init Project/app.ts
@@ -1,7 +1,44 @@
+// Type Aliases / Custom Types
+
+
+type myCustomType = number | string
+
+
+let age : myCustomType
 
 
 
- // value is 1 or 2 : this is literal type
- function funName(value: 1|2){
 
- }
+
+// ________________COPIED_______________
+
+
+// Type Aliases & Object Types
+// Type aliases can be used to "create" your own types. You're not limited to storing union types though - you can also provide an alias to a (possibly complex) object type.
+
+// For example:
+
+// type User = { name: string; age: number };
+// const u1: User = { name: 'Max', age: 30 }; // this works!
+// This allows you to avoid unnecessary repetition and manage types centrally.
+
+// For example, you can simplify this code:
+
+// function greet(user: { name: string; age: number }) {
+//   console.log('Hi, I am ' + user.name);
+// }
+ 
+// function isOlder(user: { name: string; age: number }, checkAge: number) {
+//   return checkAge > user.age;
+// }
+// To:
+
+// type User = { name: string; age: number };
+ 
+// function greet(user: User) {
+//   console.log('Hi, I am ' + user.name);
+// }
+ 
+// function isOlder(user: User, checkAge: number) {
+//   return checkAge > user.age;
+// }
