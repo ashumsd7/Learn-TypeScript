71db9164e6adf88366364419996fc5d671d6bd0f
aathym <aat8001@gmail.com>
2022-08-05 12:11:04 +0530

    Core Types : number boolean and string

--- 2. Init Project/app.js
+++ 2. Init Project/app.js
@@ -1,1 +1,7 @@
-console.log(">>>>>>>>>>>namas>>>>>>");
+function add(n1, n2) {
+    return n1 + n2;
+}
+// const number1= '2';
+var number1 = 4;
+var number2 = 2.9;
+console.log(add(number1, number2));
--- 2. Init Project/app.ts
+++ 2. Init Project/app.ts
@@ -1,1 +1,11 @@
-console.log(">>>>>>>>>>>namas>>>>>>");
+function add(n1:number,n2:number){
+    return n1+n2
+}
+
+
+// const number1= '2';
+const number1= 4;
+const number2= 2.9;
+
+
+console.log(add(number1,number2));
