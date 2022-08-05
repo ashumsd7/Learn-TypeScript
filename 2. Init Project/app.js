e3809e9396f2d968f86caa36bf661d7a2317cc48
aathym <aat8001@gmail.com>
2022-08-05 12:25:08 +0530

    use boolean and string type

--- 2. Init Project/app.js
+++ 2. Init Project/app.js
@@ -1,7 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, show, phrase) {
+    console.log(typeof n1);
+    var result = n1 + n2;
+    if (show) {
+        console.log(phrase + result);
+    }
+    return result;
 }
 // const number1= '2';
 var number1 = 4;
 var number2 = 2.9;
-console.log(add(number1, number2));
+console.log(add(number1, number2, true, "Result is: "));
--- 2. Init Project/app.ts
+++ 2. Init Project/app.ts
@@ -1,11 +1,16 @@
-function add(n1:number,n2:number){
-    return n1+n2
+function add(n1: number, n2: number, show: boolean, phrase:string) {
+  console.log(typeof n1);
+
+  let result = n1+n2
+
+  if (show) {
+    console.log(phrase+  result);
+  }
+  return result;
 }
 
-
 // const number1= '2';
-const number1= 4;
-const number2= 2.9;
+const number1 = 4;
+const number2 = 2.9;
 
-
-console.log(add(number1,number2));
+console.log(add(number1, number2, true, "Result is: "));
